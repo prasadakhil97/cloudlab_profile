aea6b081fefa041802a0160f6db0238d9d3f9c4f
Umakant Kulkarni <UmakantKulkarni@users.noreply.github.com>
2022-02-16 18:07:25 -0500

    Update setup-grow-rootfs.sh

--- setup-grow-rootfs.sh
+++ setup-grow-rootfs.sh
@@ -5,11 +5,25 @@
 #
 set -x
 
+if [ -z "$EUID" ]; then
+    EUID=`id -u`
+fi
 
-if [ `id -u` -ne 0 ] ; then
+if [ $EUID -ne 0 ] ; then
     echo "This script must be run as root" 1>&2
     exit 1
 fi
+
+# Grab our libs
+DIRNAME=`dirname $0`
+if [ -e $DIRNAME/setup-lib.sh ]; then
+    . $DIRNAME/setup-lib.sh
+else
+    logtstart() /bin/true
+    logtend() /bin/true
+fi
+
+logtstart "grow-rootfs"
 
 RESIZEROOT=0
 if [ "$#" -eq  "0" ]
@@ -26,7 +40,7 @@ fi
 echo "RESIZEROOT value is $RESIZEROOT"
 
 if [ -z "$RESIZEROOT" ]; then
-    echo "ERROR: must define RESIZEROOT to the new total size (GB) you want for the rootfs!"
+    logtend "ERROR: must define RESIZEROOT to the new total size (GB) you want for the rootfs!"
     exit 0
 fi
 if [ -z "$IMPOTENT" ]; then
@@ -38,7 +52,7 @@ fi
 
 # Remove any existing temp files.
 rm -fv /tmp/sfdisk.orig /tmp/sfdisk.new /tmp/sfdisk.new \
-/tmp/sfdisk.nextstart /tmp/sfdisk.parts-to-delete
+    /tmp/sfdisk.nextstart /tmp/sfdisk.parts-to-delete
 
 # Find the root partition's parent disk.
 eval `lsblk -n -P -b -o NAME,FSTYPE,MOUNTPOINT,PARTTYPE,PARTUUID,TYPE,PKNAME,SIZE | grep 'MOUNTPOINT="/"'`
@@ -66,63 +80,62 @@ if [ $NODELETE -eq 0 ]; then
     # Swapoff all swap devices if we are not impotent; they will be
     # removed.
     for dev in `blkid -t TYPE=swap | cut -d: -f1 | xargs` ; do
-        if [ ! $IMPOTENT -eq 1 ]; then
-            swapoff $dev
-            if [ ! $? -eq 0 ]; then
-                echo "ERROR: failed to swapoff $dev; aborting!"
-                exit 1
-            fi
-        fi
+	if [ ! $IMPOTENT -eq 1 ]; then
+	    swapoff $dev
+	    if [ ! $? -eq 0 ]; then
+		echo "ERROR: failed to swapoff $dev; aborting!"
+		exit 1
+	    fi
+	fi
     done
-    
+
     # Figure out which partitions to remove.  We remove any partition on
     # the rootparent with FSTYPE="" and MOUNTPOINT="" and
     # PARTUUID=(0fc63daf-8483-4772-8e79-3d69d8477de4|00000000-0000-0000-0000-000000000000|0657FD6D-A4AB-43C4-84E5-0933C84B4F4F|0x83|0x82|0x0).
-    
+
     PARTS=""
     lsblk -a -n -P -b -o NAME,FSTYPE,MOUNTPOINT,PARTTYPE,PARTUUID,TYPE,PKNAME,SIZE | grep "PKNAME=\"${ROOTPARENT}\"" | while read line ; do
-        eval "$line"
-        if [ "$FSTYPE" != swap -a \( -n "$FSTYPE" -o -n "$MOUNTPOINT" \) ]; then
-            continue
-        fi
-        echo "$PARTTYPE" | grep -qEi '^(0fc63daf-8483-4772-8e79-3d69d8477de4|00000000-0000-0000-0000-000000000000|0657FD6D-A4AB-43C4-84E5-0933C84B4F4F|0x83|0x82|0x0)$'
-        if [ ! $? -eq 0 ]; then
-            continue
-        fi
-        # Now extract the partition number (to feed to parted).  Partition
-        # number is not reported by most Linux tools nor by sysfs, so we
-        # have to extract via regexp.  Right now we only worry about nvme
-        # devices (or any device that ends with a "p\d+"), and assume that
-        # anything else is "standard".
-        PARTNO=`echo "$NAME" | sed -ne "s/^${PKNAME}p\([0-9]*\)$/\1/p"`
-        if [ ! $? -eq 0 -o -z "$PARTNO" ]; then
-            PARTNO=`echo "$NAME" | sed -ne "s/^${PKNAME}\([0-9]*\)$/\1/p"`
-        fi
-        if [ ! $? -eq 0 -o -z "$PARTNO" ]; then
-            continue
-        fi
-        PARTS="$PARTNO $PARTS"
-        echo $PARTNO >> /tmp/sfdisk.parts-to-delete
+	eval "$line"
+	if [ "$FSTYPE" != swap -a \( -n "$FSTYPE" -o -n "$MOUNTPOINT" \) ]; then
+	    continue
+	fi
+	echo "$PARTTYPE" | grep -qEi '^(0fc63daf-8483-4772-8e79-3d69d8477de4|00000000-0000-0000-0000-000000000000|0657FD6D-A4AB-43C4-84E5-0933C84B4F4F|0x83|0x82|0x0)$'
+	if [ ! $? -eq 0 ]; then
+	    continue
+	fi
+	# Now extract the partition (to feed to sfdisk).  Partition
+	# number is not reported by most Linux tools nor by sysfs, so we
+	# have to extract via regexp.  Right now we only worry about nvme
+	# devices (or any device that ends with a "p\d+"), and assume that
+	# anything else is "standard".
+	PART=`echo "$NAME" | sed -ne "s/^${PKNAME}\(p[0-9]*\)$/\1/p"`
+	if [ ! $? -eq 0 -o -z "$PART" ]; then
+	    PART=`echo "$NAME" | sed -ne "s/^${PKNAME}\([0-9]*\)$/\1/p"`
+	fi
+	if [ ! $? -eq 0 -o -z "$PART" ]; then
+	    continue
+	fi
+	echo $PART >> /tmp/sfdisk.parts-to-delete
     done
-    
+
     if [ -e /tmp/sfdisk.parts-to-delete ]; then
-        PARTS=`cat /tmp/sfdisk.parts-to-delete | xargs`
-        rm -f /tmp/sfdisk.tmp
-        cat /tmp/sfdisk.new | while read line ; do
-            delete=0
-            for part in $PARTS ; do
-                echo "$line" | grep -q "^/dev/${ROOTPARENT}$part :"
-                if [ $? -eq 0 ]; then
-                    delete=1
-                    break
-                fi
-            done
-            if [ $delete -eq 0 ]; then
-                echo "$line" >> /tmp/sfdisk.tmp
-            fi
-        done
-        diff -u /tmp/sfdisk.new /tmp/sfdisk.tmp
-        mv /tmp/sfdisk.tmp /tmp/sfdisk.new
+	PARTS=`cat /tmp/sfdisk.parts-to-delete | xargs`
+	rm -f /tmp/sfdisk.tmp
+	cat /tmp/sfdisk.new | while read line ; do
+	    delete=0
+	    for part in $PARTS ; do
+		echo "$line" | grep -q "^/dev/${ROOTPARENT}$part :"
+		if [ $? -eq 0 ]; then
+		    delete=1
+		    break
+		fi
+	    done
+	    if [ $delete -eq 0 ]; then
+		echo "$line" >> /tmp/sfdisk.tmp
+	    fi
+	done
+	diff -u /tmp/sfdisk.new /tmp/sfdisk.tmp
+	mv /tmp/sfdisk.tmp /tmp/sfdisk.new
     fi
 fi
 
@@ -141,11 +154,11 @@ nextstart=$DISKSIZE
 cat /tmp/sfdisk.new | grep "^/dev" | while read line ; do
     nstart=`echo $line | sed -ne "s|/dev/[^ ]* *: *start= *\([0-9]*\),.*$|\1|p"`
     if [ -z "$nstart" ] ; then
-        continue
+	continue
     fi
     if [ $nstart -gt $ROOTSTART -a $nstart -lt $nextstart ]; then
-        nextstart=$nstart
-        echo $nextstart > /tmp/sfdisk.nextstart
+	nextstart=$nstart
+	echo $nextstart > /tmp/sfdisk.nextstart
     fi
 done
 if [ -e /tmp/sfdisk.nextstart -a -s /tmp/sfdisk.nextstart ]; then
@@ -167,9 +180,9 @@ if [ $RESIZEROOT -eq 0 ]; then
 else
     usersectors=`expr $RESIZEROOT \* 1024 \* 1024 \*  1024 / 512`
     if [ $usersectors -gt $maxsize ]; then
-        newsize=$maxsize
+	newsize=$maxsize
     else
-        newsize=$usersectors
+	newsize=$usersectors
     fi
 fi
 if [ -z "$newsize" ]; then
@@ -193,9 +206,9 @@ fi
 cat /tmp/sfdisk.new | while read line ; do
     echo "$line" | grep -q "^/dev/${ROOT} :"
     if [ $? -eq 0 ]; then
-        echo "$line" | sed -e "s|^\(/dev/${ROOT} :.*\)\(size= *[0-9]*,\)\(.*\)$|\1size=${newsize}\3|" >> /tmp/sfdisk.tmp
+	echo "$line" | sed -e "s|^\(/dev/${ROOT} :.*\)\(size= *[0-9]*,\)\(.*\)$|\1size=${newsize}\3|" >> /tmp/sfdisk.tmp
     else
-        echo "$line" >> /tmp/sfdisk.tmp
+	echo "$line" >> /tmp/sfdisk.tmp
     fi
 done
 mv /tmp/sfdisk.tmp /tmp/sfdisk.new
@@ -215,5 +228,7 @@ if [ ! $? -eq 0 ]; then
 fi
 
 echo "Resized /dev/$ROOT."
+
+logtend "grow-rootfs"
 
 exit 0
